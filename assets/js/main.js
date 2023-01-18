d2ce5fcf16626cfd99994694b014a3d6d7141fc8
MohammadHaiderShafiquue <73246985+MohammadHaiderShafiquue@users.noreply.github.com>
2023-01-18 09:19:52 +0500

    designed notification box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
+//pop-box close button functionality
+
 let button=document.getElementById('close');
 button.addEventListener('click',function(){
-button.style.display="none";
-
+let button2=document.getElementById('close-all');
+button2.style.display="none";
 
 
 });
+//notification-box close button functionality
+
+let button3=document.getElementById('cross');
+button3.addEventListener('click',function(){
+    let button4=document.getElementById('notification');
+    button4.style.display="none";
+});
+//notification-box do-not-allow functionality
+let button5=document.getElementById('do-not-allow');
+button5.addEventListener('click',function(){
+let button6=document.getElementById('notification');
+button6.style.display="none";
+
+});
+
+
+
+
+
